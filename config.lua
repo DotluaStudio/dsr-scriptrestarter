9a3cfe68e50ec7c5f86c66bc5ccf0ac758d21173
DotluaStudio <jens.ferre@yahoo.com>
2024-11-22 13:16:01 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+scriptsToRestart = {
+    'script_example_one',
+    'script_example_two',
+    -- Add more script names here
+}
+
+restartTimer = 60000  -- Set desired timer in milliseconds (currently wait one minute after server re-/start before restarting scripts listed above)
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Dotlua Studio'
+description 'Script restarter'
+version '1.0.0'
+
+shared_script 'config.lua'
+server_script 'main.lua'
+
+lua54 'yes'
