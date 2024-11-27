aab2acd86daf0a81abd6a088034b56268c46c52d
DotluaStudio <jens.ferre@yahoo.com>
2024-11-27 01:19:12 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = {}
 
 scriptsToRestart = {
-    'script_example_one',
-    'script_example_two',
-    -- Add more script names here
+    'script_example_one', -- First script to respawn
+    'script_example_two', -- Second script to respawn
+    -- Add more scripts here
 }
 
 restartTimer = 60000  -- Set desired timer in milliseconds (currently wait one minute after server re-/start before restarting scripts listed above)
